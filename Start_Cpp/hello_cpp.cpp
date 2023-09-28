f0b15ed175b3d6258188ba105b653996a3c57752
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-09-28 13:27:59 +0200

    confiured outpu in plain text

--- Start_Cpp/hello_cpp.cpp
+++ Start_Cpp/hello_cpp.cpp
@@ -3,12 +3,18 @@ using namespace std;
 
 
 int main() {
-    int x, y;
-    cin >> x >> y;
-    cout << x << " " << y;
+    // int x;
+    // int y;
+    // long long r = 5;
+    string str;
+    getline(cin, str);
+    cout << str << " " << "\n";
 
+    // cin >> x >> y;
+    // cout << x << " " << y << "\n";
+    // cout << r << " " << "\n";
 
-
+    // float n = 5.2;
 
     return 0;
 }

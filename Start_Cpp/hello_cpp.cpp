6395570b587e44ac0aeddbdea45dbbcb44fdbe71
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-09-28 18:44:15 +0200

    solve patterns

--- Start_Cpp/hello_cpp.cpp
+++ Start_Cpp/hello_cpp.cpp
@@ -1,20 +1,115 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void printP1(int n) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+void printP2(int n) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+void printP3(int n) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j <<" ";
+        }
+        cout << endl;
+    }
+}
+void printP4(int n) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i <<" ";
+        }
+        cout << endl;
+    }
+}
+void printP5(int n) {
+    for (int i = 0; i <= n; i++)
+    {
+        for(int j=0; j<=n-i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+void printP6(int n) {
+    for (int i = 1; i <= n; i++)
+    {
+        for(int j=1; j<=n-i+1; j++) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+void printP7(int n) {
+    for (int i = 0; i<n; i++) {
+        for(int j=0; j<n-i-1; j++) {
+            // Space
+            cout << " ";
+        }
+
+        for (int k=0; k<2*i+1; k++) {
+            // Stars
+            cout << "*";
+        }
+
+        for (int l=0; l<n-i-1; l++) {
+            // Spaces
+            cout << " ";
+        }
+        cout << endl;
+    }
+    
+}
+void printP8(int n) {
+    for (int i = 0; i<n; i++) {
+
+        for(int j=0; j<i; j++) {
+            // Space
+            cout << " ";
+        }
+
+        for (int k=0; k<2*(n-i)-1; k++) {
+            // Stars
+            cout << "*";
+        }
+
+        for (int l=0; l<i; l++) {
+            // Spaces
+            cout << " ";
+        }
+        cout << endl;
+    }
+    
+}
 
 int main() {
-    // int x;
-    // int y;
-    // long long r = 5;
-    string str;
-    getline(cin, str);
-    cout << str << " " << "\n";
+    int t;
+    cin >> t;
 
-    // cin >> x >> y;
-    // cout << x << " " << y << "\n";
-    // cout << r << " " << "\n";
-
-    // float n = 5.2;
+    for(int i=0; i<t; i++) {
+        int n;
+        cin >> n;
+        printP8(n);
+    }
 
     return 0;
 }

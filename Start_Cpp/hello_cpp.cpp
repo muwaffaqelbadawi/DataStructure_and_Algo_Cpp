7466459db858be89d93cb8ca910c25dbf12ba89f
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-09-29 10:15:50 +0200

    Fridaygit add .

--- Start_Cpp/hello_cpp.cpp
+++ Start_Cpp/hello_cpp.cpp
@@ -1,7 +1,8 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void printP1(int n) {
+void printP1(int n)
+{
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -11,7 +12,8 @@ void printP1(int n) {
         cout << endl;
     }
 }
-void printP2(int n) {
+void printP2(int n)
+{
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j <= i; j++)
@@ -21,96 +23,196 @@ void printP2(int n) {
         cout << endl;
     }
 }
-void printP3(int n) {
+void printP3(int n)
+{
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= i; j++)
         {
-            cout << j <<" ";
-        }
-        cout << endl;
-    }
-}
-void printP4(int n) {
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= i; j++)
-        {
-            cout << i <<" ";
-        }
-        cout << endl;
-    }
-}
-void printP5(int n) {
-    for (int i = 0; i <= n; i++)
-    {
-        for(int j=0; j<=n-i; j++) {
-            cout << "* ";
-        }
-        cout << endl;
-    }
-}
-void printP6(int n) {
-    for (int i = 1; i <= n; i++)
-    {
-        for(int j=1; j<=n-i+1; j++) {
             cout << j << " ";
         }
         cout << endl;
     }
 }
-void printP7(int n) {
-    for (int i = 0; i<n; i++) {
-        for(int j=0; j<n-i-1; j++) {
+void printP4(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+void printP5(int n)
+{
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n - i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+void printP6(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+void printP7(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
             // Space
             cout << " ";
         }
 
-        for (int k=0; k<2*i+1; k++) {
+        for (int k = 0; k < 2 * i + 1; k++)
+        {
             // Stars
             cout << "*";
         }
 
-        for (int l=0; l<n-i-1; l++) {
+        for (int l = 0; l < n - i - 1; l++)
+        {
             // Spaces
             cout << " ";
         }
         cout << endl;
     }
-    
 }
-void printP8(int n) {
-    for (int i = 0; i<n; i++) {
+void printP8(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
 
-        for(int j=0; j<i; j++) {
+        for (int j = 0; j < i; j++)
+        {
             // Space
             cout << " ";
         }
 
-        for (int k=0; k<2*(n-i)-1; k++) {
+        for (int k = 0; k < 2 * (n - i) - 1; k++)
+        {
             // Stars
             cout << "*";
         }
 
-        for (int l=0; l<i; l++) {
+        for (int l = 0; l < i; l++)
+        {
             // Spaces
             cout << " ";
         }
         cout << endl;
     }
-    
 }
+void printP9(int n)
+{
+    for (int i = 0; i <= n; i++)
+    {
 
-int main() {
+        for (int j = 0; j < n - 1; j++)
+        {
+            // Space
+            cout << " ";
+        }
+
+        for (int k = 0; k < i; k++)
+        {
+            // Stars
+            cout << "*";
+        }
+
+        for (int l = 0; l < n - i; l++)
+        {
+            // Spaces
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void printP10(int n)
+{
+    for (int i = 0; i <= n; i++)
+    {
+
+        for (int j = 0; j < n - 1; j++)
+        {
+            // Space
+            cout << " ";
+        }
+
+        for (int k = 0; k < n - i + 1; k++)
+        {
+            // Stars
+            cout << "*";
+        }
+
+        for (int l = 0; l < i; l++)
+        {
+            // Spaces
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void printP101(int n)
+{
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
+        int stars_num = i;
+        if (i > n)
+            stars_num = 2 * n - i;
+
+        for (int j = 1; j <= stars_num; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+}
+void printP11(int n)
+{
+    int start = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+            start = 1;
+        else
+            start == 0;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << start;
+            start = 1 - start;
+        }
+        cout << endl;
+    }
+    
+};
+
+int main()
+{
     int t;
     cin >> t;
 
-    for(int i=0; i<t; i++) {
+    for (int i = 0; i < t; i++)
+    {
+        // Test Cases
         int n;
         cin >> n;
-        printP8(n);
+        printP11(n);
     }
 
     return 0;
 }
-

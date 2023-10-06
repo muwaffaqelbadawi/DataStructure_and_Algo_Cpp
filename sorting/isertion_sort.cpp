f45eeaf4dada758d34c485333342906da39788b7
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-10-06 11:56:16 +0200

    understanding merge sort

--- sorting/isertion_sort.cpp
+++ sorting/isertion_sort.cpp
@@ -55,7 +55,7 @@ void insersion_sort(int arr[], int n)
         }
     }
 }
-
+/* 
 ///////////////////////////////////////////////////////////////////////////
 void merge(vector<int> &arr, int low, int mid, int high)
 {
@@ -103,19 +103,61 @@ void mS(vector<int> &arr, int low, int high)
 void mergeSort(vector<int> &arr, int n)
 {
     mS(arr, 0, n - 1);
+} */
+
+void merge(vector<int> &arr, int low, int mid, int high)
+{
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+    while(left <= mid && right <= high) {
+        if(arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else 
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while(left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for(int i=low; i <= high; i++) {
+        arr[i] = temp[i-high];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high) {
+    if(low >= high) return;
+    int mid = (low + high) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
 }
 
 int main()
 {
     int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-        mergeSort(arr, n);
 
-    for (int i = 0; i < n; i++)
+    vector<int> arr;
+    for (int i = 0; i < arr.size() -1; i++)
     {
-        cout << arr[i] << " ";
+        cin >> n;
+        arr.push_back(n);
+    }
+
+    mergeSort(arr, 0, arr.size() - 1);
+
+    for (auto it: arr)
+    {
+        cout << it << " ";
     }
 
     return 0;

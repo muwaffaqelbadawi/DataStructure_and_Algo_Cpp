f4fd47db5b3e95ae6a93207bb7f5f9b2e2a92a52
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-10-11 17:27:44 +0200

    majorityElement optimal solution

--- Arrays/ifArraySorted/arraySorted.cpp
+++ Arrays/ifArraySorted/arraySorted.cpp
@@ -168,6 +168,30 @@ int majorityElement(vector<int> v)
     }
     return -1;
 }
+int majorityElement(vector<int> v) {
+	int count = 0;
+	int el;
+	int n = v.size();
+	for(int i=0; i<n; i++) {
+		if(count == 0) {
+			count = 1;
+			el = v[i];
+		}
+		else if(v[i] == el) count++;
+		else count--;
+	}	
+
+	int count1 = 0;
+	for(int i=0; i<n; i++) {
+		if(v[i] == el) count1++;
+	}
+	if(count1 > (n / 2)) return el;
+	else return -1;
+}
+
+
+
+
 
 int main()
 {

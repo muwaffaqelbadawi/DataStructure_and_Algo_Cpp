d560dca624acb544658e435cd8c869997543e57b
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-10-24 16:24:29 +0200

    Three some

--- Arrays/Rotate Matrix by 90 degrees/rotateMatrixBy90.cpp
+++ Arrays/Rotate Matrix by 90 degrees/rotateMatrixBy90.cpp
@@ -248,6 +248,42 @@ vector<int> majorityElementII(vector<int> v)
     return ls;
 }
 
+
+// Three some - Brute
+vector<vector<int>> triplet(int n, vector<int> &arr)
+{
+    set<vector<int>> st;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+            {
+                if (arr[i] + arr[j] + arr[k] == 0)
+                {
+                    vector<int> temp = {arr[i], arr[j], arr[k]};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+            }
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
+
+// Three some - Better Solution
+
+
+
+
+
+
+
+
+
+
+
 int main()
 {
     int n;

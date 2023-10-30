1038657bf1f12e4c380f21f1d2f170387bfbd0ca
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-10-30 16:08:21 +0200

    Merge All Overlapping Intervals - Better & O.P.

--- Arrays/ifArraySorted/arraySorted.cpp
+++ Arrays/ifArraySorted/arraySorted.cpp
@@ -150,7 +150,6 @@ string read(int n, vector<int> book, int target)
     return "NO";
 }
 
-
 // Find longest subarray with sum K
 int getLongestSubarray(vector<int> &a, int k)
 {
@@ -286,6 +285,62 @@ vector<int> rearrangeArray(vector<int> &nums)
         {
             ans[posIndex] = nums[i];
             posIndex += 2;
+        }
+    }
+    return ans;
+}
+
+// Merge All Overlapping Intervals - Brute
+vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr)
+{
+    int n = arr.size();
+    sort(arr.begin(), arr.end());
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n; i++)
+    {
+        int start = arr[i][0];
+        int end = arr[i][1];
+
+        if (!ans.empty() && end <= ans.back()[1])
+        {
+            continue;
+        }
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j][0] <= end)
+            {
+                end = max(end, arr[j][1]);
+            }
+
+            else
+            {
+                break;
+            }
+        }
+        ans.push_back({start, end});
+    }
+    return ans;
+}
+
+// Merge All Overlapping Intervals - O.P.
+vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr)
+{
+    int n = arr.size();
+    sort(arr.begin(), arr.end());
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (ans.empty() || arr[i][0] > ans.back()[1])
+        {
+            ans.push_back(arr[i]);
+        }
+
+        else
+        {
+            ans.back()[1] = max(ans.back()[1], arr[i][1]);
         }
     }
     return ans;

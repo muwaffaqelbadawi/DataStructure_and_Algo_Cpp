b40b633a5aa297d9f5962b3a00466b6acbeada9e
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-11-05 14:30:56 +0200

    Windows task

---
+++ Arrays/Binary Search/Intro/BS.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Greet()
+{
+    cout << "Hello";
+}
+
+
+
+
+int main()
+{
+    vector<int> arr = {2,3,-2,4};
+
+    Greet();
+
+
+
+    return 0;
+}

89b9057c173a12df625ec19d9216d34d1bc69746
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-11-06 17:31:13 +0200

    Removed a folder and intro to Binary Search STL

--- Arrays/Binary_Search/BS.cpp
+++ Arrays/Binary_Search/BS.cpp
@@ -1,20 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-void Greeting()
+// Binary Search - Iterative approach
+int searchIterative(vector<int> &nums, int target)
 {
-    cout << "Hello" << endl;
+    int n = nums.size();
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+
+        else if (target > nums[mid])
+        {
+            low = mid + 1;
+        }
+
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
 }
 
+// Binary Search - Recursive approach
+int bs(vector<int> &nums, int low, int high, int target)
+{
+    if (low > high)
+    {
+        return -1;
+    }
 
+    int mid = (low + high) / 2;
+    if (nums[mid] == target)
+    {
+        return mid;
+    }
+
+    else if (target > nums[mid])
+    {
+        return bs(nums, mid + 1, high, target);
+    }
+
+    else
+    {
+        return bs(nums, low, mid - 1, target);
+    }
+}
+
+int searchRecursive(vector<int> &nums, int target)
+{
+    return bs(nums, 0, nums.size() - 1, target);
+}
 
 int main()
 {
-    vector<int> arr = {2,3,-2,4};
+    int n;
+    vector<int> arr;
+    int target = 3;
 
-    Greeting();
+    while (cin >> n)
+    {
+        arr.push_back(n);
+    }
 
+    int result = searchIterative(arr, target);
+    cout << result;
 
     return 0;
 }

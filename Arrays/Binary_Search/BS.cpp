fbd3cb88c0afd318984e624efa0c0c0ca25ef83d
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-11-06 14:11:41 +0200

    I configured code-runner and task for Windows Finally

---
+++ Arrays/Binary_Search/BS.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void Greeting()
+{
+    cout << "Hello" << endl;
+}
+
+
+
+int main()
+{
+    vector<int> arr = {2,3,-2,4};
+
+    Greeting();
+
+
+    return 0;
+}

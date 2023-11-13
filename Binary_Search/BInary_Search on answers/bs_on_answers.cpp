7d2b29701493a5943e3bf613d872a1698f83ce30
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-11-13 09:41:17 +0200

    Aggressive Cows

--- Binary_Search/BInary_Search on answers/bs_on_answers.cpp
+++ Binary_Search/BInary_Search on answers/bs_on_answers.cpp
@@ -374,6 +374,56 @@ int missingK(vector<int> vec, int n, int k)
     // return high + k + 1
 }
 
+// Aggressive Cows
+// Can we place is a helper function
+bool canWePlace(vector<int> &stalls, int dist, int k)
+{
+    // C1 will always be at first place
+    // k = number of cows
+    int cntCows = 1;
+    int last = stalls[0];
+    int n = stalls.size();
+    
+    for(int i=0; i<n; i++)
+    {
+        if((stalls[i] - last) >= dist)
+        {
+            cntCows++;
+            last = stalls[i];
+        }
+
+        if(cntCows >= k)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int aggressiveCows(vector<int> &stalls, int k)
+{
+    sort(stalls.begin(), stalls.end());
+
+    int n = stalls.size();
+    int low = 0;
+    int high = stalls[n-1] - stalls[0];
+
+    while(low <= high)
+    {
+        int mid = (low + high) / 2;
+        if(canWePlace(stalls, mid, k) == true)
+        {
+            low = mid + 1;
+        }
+
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return high;
+}
+
 main()
 {
     int x;

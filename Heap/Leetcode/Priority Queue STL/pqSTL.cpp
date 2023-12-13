35ed2dce3792d8f79c48fb4181c67ee0dd844042
Muwaffaq <kingmoaffag10@gmail.com>
2023-12-13 08:52:19 +0200

    Take U Forward problems

--- Heap/Leetcode/Priority Queue STL/pqSTL.cpp
+++ Heap/Leetcode/Priority Queue STL/pqSTL.cpp
@@ -3,17 +3,24 @@ using namespace std;
 
 int kthLargest(int *arr, int n, int k)
 {
-    // Write your code here
     priority_queue<int> pq;
     for (int i = 0; i < n; i++)
+    {
         pq.push(arr[i]);
+    }
+        
     while (--k)
+    {
         pq.pop();
+    }
+        
     return pq.top();
 }
 
 int main()
 {
+    // vector<int> arr = {5, 6, 7, 2};
+
     int n;
     cin >> n;
 
@@ -30,9 +37,6 @@ int main()
     cout << kthLargest(arr, n, k);
 
     delete[] arr;
-
-    vector<int> arr = {5, 6, 7, 2};
-    // 2 5 6 7
 
     return 0;
 }

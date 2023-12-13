5df94a086e365b8f4ed86394ccd15582978d3637
Muwaffaq <kingmoaffag10@gmail.com>
2023-12-13 09:52:09 +0200

    Changed to gnu++23

---
+++ Heap/TakeUForward problems/problem.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void kth_Largest_MaxHeap(vector<int> &arr, int k)
+{
+    priority_queue<int> pq;
+    int n = arr.size();
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        pq.push(arr[i]);
+    }
+
+    int f = k - 1;
+
+    while (f > 0)
+    {
+        pq.pop();
+        f--;
+    }
+
+    cout << "Kth Largest element " << pq.top() << "\n";
+}
+
+void kth_Smallest_MinHeap(vector<int> &arr, int k)
+{
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int n = arr.size();
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        pq.push(arr[i]);
+    }
+
+    int f = k - 1;
+
+    while (f > 0)
+    {
+        pq.pop();
+        f--;
+    }
+
+    cout << "Kth Smallest element " << pq.top() << "\n";
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 6, 4, 5, 3};
+    // 1 2 3 4 5 6
+
+    // kth_Largest_MaxHeap(arr, 3); //4
+    kth_Smallest_MinHeap(arr, 3); //3
+
+    return 0;
+}
